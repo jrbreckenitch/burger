ec07c8276211046eb8bce94b1eacbd7042b1b234
jrbreckenitch <jamesbreckenitch@Jamess-MacBook-Pro.local>
2018-10-10 21:07:01 -0500

    updated prelim files

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon cheeseburger', false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Black bean burger', false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie burger', false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Turkey burger', true);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double cheeseburger', true);
+
+
