e4e294bd47afc0c35319decc6b9c4ec0f41e398c
jrbreckenitch <jamesbreckenitch@Jamess-MacBook-Pro.local>
2018-10-12 18:39:32 -0500

    cleaned up file naming error

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var express = require('express');
-
+var exphbs = require("express-handlebars");
+var app = express();
+var routes = require("./controllers/burgers_controller.js");
 var PORT = process.env.PORT || 8080;
 
-var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
@@ -12,14 +13,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Set Handlebars.
-var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/burgers_ontroller.js");
 
+// app.get('/', (req, res) => {
+//   res.render('index');
+// })
 app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
